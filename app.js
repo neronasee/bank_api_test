4e1fc86e19abfbe236013b11cb6940da9627c79e
Sasha Vorona <sashavorona@MacBook-Pro-Sasha.local>
2017-09-10 15:18:56 +0300

    boilerplate commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcom",
+}));
+
+module.exports = app;
